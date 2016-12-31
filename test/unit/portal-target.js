79cdb99dc9275531a6ddf6e69ea095e589a83a7e
Thorsten <t.luenborg@googlemail.com>
2016-12-31 13:34:20 +0100

    improving tests.

--- test/unit/portal-target.js
+++ test/unit/portal-target.js
@@ -1,5 +1,20 @@
 import test from 'ava'
+import Wormhole from '../../src/components/wormhole'
+import Portal from '../../src/components/portal'
 
-import Portal from '../src/components/portal'
+const methods = ['sendUpdate', 'get', 'clear']
 
-test.todo('Tests for Portal component')
+// stub wormhole
+test.before(t => {
+  methods.forEach(method => {
+    sinon.stub(Wormhole, method)
+  })
+})
+
+test.after(t => {
+  methods.forEach(method => {
+    Wormhole[method].restore()
+  })
+})
+
+test.todo('Tests for PortalTarget component')
