9cc9a3c6161e0d3c94e4e15d9bcc1710bdb5afe4
Thorsten <t.luenborg@googlemail.com>
2017-01-05 11:32:06 +0100

    enable hot reload and prod/dev variables.

--- build/webpack.conf.js
+++ build/webpack.conf.js
@@ -38,6 +38,12 @@ const config = {
     ]
   },
   plugins: [
+    new webpack.HotModuleReplacemendPlugin(),
+    new webpack.DefinePlugin({
+      process.env: {
+        NODE_ENV: process.env.NODE_ENV ? JSON.stringify(process.env.NODE_ENV) : "'development'"
+      }
+    }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
     })
