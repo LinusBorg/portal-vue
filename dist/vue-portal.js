f03149ccfd59b8927e48a893dc9cf6d9219af5e2
Thorsten <t.luenborg@googlemail.com>
2017-02-12 14:49:07 +0100

    last build

--- dist/vue-portal.js
+++ dist/vue-portal.js
@@ -24,50 +24,36 @@ var wormhole = new Vue({
   }
 });
 
-var Portal = {
-	name: 'portal',
-  props: {
-    to: { type: String, required: true },
-    mountTo: { type: String },
-    tag: {type: String, default: 'div'},
-  },
-  beforeMount: function beforeMount() {
-  	this.sendUpdate();
-  },
-  beforeDestroy: function beforeDestroy() {
-    wormhole.clear(this.to);
-  },
-  updated: function updated() {
-		this.sendUpdate();
-	},
-  methods: {
-  	sendUpdate: function sendUpdate() {
-    	wormhole.sendUpdate(this.to, this.$slots.default);
-    },
-  },
-  render: function render(h) {
-  	return  null
-  }
-};
-
-var PortalTarget = {
+var Target = {
 	name: 'portalTarget',
   props: {
     name: { type: String, required: true },
-    tag: {type: String, default: 'div' }
+		id: { type: String },
+    tag: { type: String, default: 'div' },
   },
-  beforeMount: function beforeMount() {
+
+	beforeMount: function beforeMount() {
 		this.checkWormhole();
   	wormhole.$on(this.name, this.update);
   },
   beforeDestroy: function beforeDestroy() {
+		this.$el.innerHTML = '';
   	wormhole.$off(this.name, this.update);
   },
-  methods: {
+
+	watch: {
+		name: function name(newName, oldName) {
+			wormhole.$off(oldName, this.update);
+			wormhole.$on(newName, this.update);
+			this.checkWormhole();
+		}
+	},
+
+	methods: {
 
 		checkWormhole: function checkWormhole() {
 			var passengers = wormhole.get(this.name);
-			passengers && this.update(passengers);
+			this.update(passengers);
 		},
 
 		update: function update(passengers) {
@@ -80,10 +66,82 @@ var PortalTarget = {
       this.$forceUpdate(); // force re-render
     }
   },
-  render: function render(h) {
+
+	render: function render(h) {
   	return h(this.tag, {
-    	class: { 'vue-portal-target': true }
+    	class: { 'vue-portal-target': true },
+			attrs: {
+				id: this.id && this.id.substr(1) || false,
+			}
     }, this.passengers)
+  }
+};
+
+var Portal = {
+	name: 'portal',
+  props: {
+    to: { type: String, required: true },
+    mountTarget: { type: String },
+  },
+
+	beforeMount: function beforeMount() {
+		if (this.mountTarget) {
+			this.mountToTarget();
+		}
+  	this.sendUpdate();
+  },
+  beforeUpdate: function beforeUpdate() {
+		this.sendUpdate();
+	},
+	beforeDestroy: function beforeDestroy() {
+		wormhole.clear(this.to);
+		if (this.mountedComp) {
+			this.mountedComp.$destroy();
+		}
+	},
+
+	watch: {
+		to: function to (newValue, oldValue) {
+			oldValue && wormhole.sendUpdate(oldValue, null);
+			this.sendUpdate();
+		},
+		mountTarget: function mountTarget (newValue, oldValue) {
+			this.mountToTarget();
+		}
+	},
+
+	methods: {
+
+		sendUpdate: function sendUpdate() {
+			if (this.to) {
+				wormhole.sendUpdate(this.to, this.$slots.default);
+			} else {
+				console.warn('[vue-portal]: You have to define a targte via the `to` prop.');
+			}
+    },
+
+		mountToTarget: function mountToTarget() {
+			var el = document.querySelector(this.mountTarget);
+
+			if (el) {
+
+				var target = new Vue(Object.assign({}, Target,
+					{propsData: {
+						name: this.to || Math.round(Math.random() * 10000),
+						id: this.mountTarget,
+						tag: el.tagName,
+					}}));
+				target.$mount(el);
+				this.mountedComp = target;
+
+			} else {
+				console.warn('[vue-porta]: The specified mountTarget ' + this.mountTarget + ' was not found');
+			}
+		}
+  },
+
+	render: function render(h) {
+  	return  null
   }
 };
 
@@ -92,13 +150,13 @@ function install(Vue$$1, opts) {
 
 
   Vue$$1.component(opts.portalName || 'portal', Portal);
-  Vue$$1.component(opts.portalTargetName || 'portal-target', PortalTarget);
+  Vue$$1.component(opts.portalTargetName || 'portal-target', Target);
 }
 
 var index = {
   install: install,
   Portal: Portal,
-  PortalTarget: PortalTarget
+  PortalTarget: Target
 };
 
 return index;
