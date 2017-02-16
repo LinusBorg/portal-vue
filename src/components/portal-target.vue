ca61cf8ea854036af76a2914c12d634cd9e7beeb
Thorsten <t.luenborg@googlemail.com>
2017-02-16 21:35:15 +0100

    fix attributes application: has to be done after render.

--- src/components/portal-target.vue
+++ src/components/portal-target.vue
@@ -13,10 +13,40 @@
 				routes,
 			}
 		},
+
+		mounted() {
+			this.updateAttributes()
+		},
+		updated() {
+			this.updateAttributes()
+		},
 		beforeDestroy() {
 			this.$el.innerHTML = ''
 		},
 
+		methods: {
+			updateAttributes() {
+				if (this.attributes) {
+
+					const attrs = this.attributes
+					const el = this.$el
+
+					// special treatment for class
+					if(attrs.class) {
+						attrs.class.trim().split(' ').forEach((klass) => {
+								el.classList.add(klass)
+						})
+						delete attrs.class
+					}
+
+					const keys = Object.keys(attrs)
+
+					for (let i = 0; i < keys.length; i++) {
+						el.setAttribute(keys[i], attrs[keys[i]])
+					}
+				}
+			}
+		},
 		computed: {
 			passengers() {
 
@@ -32,8 +62,7 @@
 			}
 			else {
 				return h(this.tag, {
-					class: { 'vue-portal-target': true },
-					attrs: this.attributes,
+					class: { 'vue-portal-target': true }
 				}, children)
 			}
 		}
