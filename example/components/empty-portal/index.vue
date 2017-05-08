05ed917bda02986e7d1c23871d2b65b612a5be38
Thorsten <t.luenborg@googlemail.com>
2017-05-08 22:23:44 +0200

    change „empty portal“ example to include an empty text node

--- example/components/empty-portal/index.vue
+++ example/components/empty-portal/index.vue
@@ -2,17 +2,19 @@
   <div id="empty-portal-example">
     <div class="wrapper">
       <div class="item source">
-        <portal to="empty-portal-destination"></portal>
+        <portal to="empty-portal-destination">
+
+        </portal>
       </div>
       <div class="item destination">
-        <portal-target name="empty-portal-destination" />
+        <portal-target name="empty-portal-destination" slim />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  // This Example is just usrd to test that an empty portal works as expected.
+  // This Example is just used to test that an empty portal works as expected.
   export default {
 
   }
