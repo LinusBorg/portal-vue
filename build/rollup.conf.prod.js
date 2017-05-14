a5c2b9146854d4ab09d6ec97ee40e173016f0a06
Thorsten <t.luenborg@googlemail.com>
2017-05-14 14:09:24 +0200

    changed build script to work around cross-env

--- build/rollup.conf.prod.js
+++ build/rollup.conf.prod.js
@@ -3,6 +3,11 @@ import vue from 'rollup-plugin-vue'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
+const pkg = require('../package.json')
+
+const version = pkg.version
+// const version = '1.0.0-beta.4'
+
 const babelConfig = {
   // runtimeHelpers: true,
   exclude: 'node_modules/**',
@@ -14,6 +19,13 @@ const nodeResolveOptions = {
 }
 
 export default {
+  banner: `/*
+    portal-vue
+    Version: ${version}
+    Licence: MIT
+    (c) Thorsten Lünborg
+  */
+  `,
   entry: './src/index.js',
   external: ['vue'],
   globals: {
