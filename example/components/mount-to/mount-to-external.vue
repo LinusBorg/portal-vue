5756e8e271f68319f79460c916f8d186e88f425f
Thorsten <t.luenborg@googlemail.com>
2017-05-20 10:24:09 +0200

    change targetEl example to use default for "to"

--- example/components/mount-to/mount-to-external.vue
+++ example/components/mount-to/mount-to-external.vue
@@ -3,7 +3,7 @@
     <h5>Mount a &lt;portal-target&gt;</h5>
     <button @click="toggle">toggle portal &amp; target</button>
     <button @click="target = ''">Unset targetEl</button>
-    <portal v-if="show" to="external" :targetEl="targetEl">
+    <portal v-if="show" :targetEl="targetEl">
       <div class="">
         <p>This content will be portal'ed to an element outside of this Vue app, with the help of the <i>targetEl</i> prop.</p>
         <test />
