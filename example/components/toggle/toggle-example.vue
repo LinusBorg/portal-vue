f3a4c96c8bff192a7346f747f11283eebb0a8d32
Thorsten <t.luenborg@googlemail.com>
2017-12-11 17:37:26 +0100

    whatever

--- example/components/toggle/toggle-example.vue
+++ example/components/toggle/toggle-example.vue
@@ -3,10 +3,10 @@
     <div>
       <ul class="controls">
         <li class="controls--item">
-          <a href="#" class="controls--link" @click.prevent="showDestination = !showDestination">Toggle Destination</a>
+          <a href="#" class="controls--link toggle-dest" @click.prevent="showDestination = !showDestination">Toggle Destination</a>
         </li>
         <li class="controls--item">
-          <a href="#" class="controls--link" @click.prevent="showSource = !showSource">Toggle Source</a>
+          <a href="#" class="controls--link toggle-source" @click.prevent="showSource = !showSource">Toggle Source</a>
         </li>
       </ul>
     </div>
