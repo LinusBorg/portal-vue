0fb175fcd02977d09d171ea546f840a62176efb0
Thorsten <t.luenborg@googlemail.com>
2018-12-29 14:33:07 +0100

    package and config improvement

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,13 @@ module.exports = {
   runtimeCompiler: true,
   chainWebpack: config => {
     const path = require('path')
+    const { main } = require('./package.json')
     config.module.rule('js').exclude.add(path.join(__dirname, './dist'))
     config.module.rule('ts').exclude.add(path.join(__dirname, './dist'))
 
     config.resolve.alias.set(
       'portal-vue$',
-      path.join(__dirname, './dist/portal-vue.esm.js')
+      path.join(__dirname, main /*'./dist/portal-vue.esm.js'*/)
     )
   },
 }
