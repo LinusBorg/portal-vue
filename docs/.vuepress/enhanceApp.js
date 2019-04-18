9440a2ac03b7986f1de5f832bea44ac7edfddd3b
Thorsten Lünborg <t.luenborg@googlemail.com>
2019-04-18 19:16:24 +0200

    docs: add vue-ko-fi-button (#197)
    Switched out the "hardcoded" button with a little component I wrote & published

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,11 +1,13 @@
 import PortalVue from '../../dist/portal-vue.esm'
 import VueResize from 'vue-resize'
 import NoSsr from 'vue-no-ssr'
+import KoFiButton from '@linusborg/vue-ko-fi-button'
 
 export default ({ Vue }) => {
   Vue.use(PortalVue)
   Vue.use(VueResize)
   Vue.component('NoSsr', NoSsr)
+  Vue.component('KoFiButton', KoFiButton)
   Vue.prototype.$__VERSION__ = process.env.VERSION
 
   if (typeof window !== 'undefined') {
