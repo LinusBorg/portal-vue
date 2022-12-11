5571de89a799ee1493e9a2555a439c22df605e48
Thorsten Luenborg <t.luenborg@googlemail.com>
2022-12-11 11:48:31 +0100

    chore: update docs build check to redeploy on change in package.json

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -3,4 +3,4 @@
 # check if doc files changes for netlify
 # needed because we cannot use && in netlify.toml
 
-git diff --quiet 'HEAD^' HEAD ./docs/ && ! git diff 'HEAD^' HEAD ./pnpm-lock.yaml | grep --quiet vite
+git diff --quiet 'HEAD^' HEAD ./docs/ && ! git diff 'HEAD^' HEAD ./pnpm-lock.yaml && ! git diff 'HEAD^' HEAD ./package.json
